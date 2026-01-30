b126ae3cbce3f410c72116b24ae0e7a9838fd428
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-01-30 12:42:03 -0500

    added menu group files

---
+++ src/main/java/com/malabudi/dineupbe/menu/model/MenuGroup.java
@@ -0,0 +1,23 @@
+package com.malabudi.dineupbe.menu.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "menu_groups")
+public class MenuGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "menuGroup", cascade = CascadeType.ALL)
+    private List<MenuItem> menuItems = new ArrayList<>();
+}
