e1c69d7ce257fef31a4b33fda87a677efb76d3f3
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-02-06 11:51:09 -0500

    added business logic for placing, fetching, and updating orders

--- src/main/java/com/malabudi/dineupbe/menu/repository/MenuItemRepository.java
+++ src/main/java/com/malabudi/dineupbe/menu/repository/MenuItemRepository.java
@@ -3,8 +3,4 @@ package com.malabudi.dineupbe.menu.repository;
 import com.malabudi.dineupbe.menu.model.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface MenuItemRepository extends JpaRepository<MenuItem, Integer> {
-    Optional<MenuItem> findByName(String name);
-}
+public interface MenuItemRepository extends JpaRepository<MenuItem, Integer> { }
