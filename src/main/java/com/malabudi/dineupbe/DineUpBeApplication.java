ae251166ca1eca6e7bdcb0bdf5c923e691abf2c0
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-02-06 15:56:53 -0500

    test: setup junit testing and added integration test flow for placing order and checking customer perm

--- src/main/java/com/malabudi/dineupbe/DineUpBeApplication.java
+++ src/main/java/com/malabudi/dineupbe/DineUpBeApplication.java
@@ -7,17 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("api/v1/")
 public class DineUpBeApplication {
-
     public static void main(String[] args) {
-        System.out.println("JWT_SECRET from env: " + System.getenv("JWT_SECRET"));
         SpringApplication.run(DineUpBeApplication.class, args);
-    }
-
-    @GetMapping
-    public String helloWorld() {
-        return "Hello World";
     }
 }
