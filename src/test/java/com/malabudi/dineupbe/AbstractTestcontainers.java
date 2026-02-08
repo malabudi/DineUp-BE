e2d1289429d92695fe4e2957bf1c92685f2aa006
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-02-07 19:01:21 -0500

    test: added basic repository tests for menu group and item

--- src/test/java/com/malabudi/dineupbe/AbstractTestcontainers.java
+++ src/test/java/com/malabudi/dineupbe/AbstractTestcontainers.java
@@ -2,14 +2,16 @@ package com.malabudi.dineupbe;
 
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class AbstractTestcontainers {
 
-    @Container
     @ServiceConnection
     protected static final PostgreSQLContainer<?> postgres =
             new PostgreSQLContainer<>("postgres:16-alpine");
+
+    static {
+        postgres.start();
+    }
 }
