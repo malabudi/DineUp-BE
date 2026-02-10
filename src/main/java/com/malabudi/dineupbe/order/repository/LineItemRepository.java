a55a75c410b2bb0bc300de51c4485da3e1ad38bf
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-02-10 13:06:55 -0500

    chore: removed unused repository file

--- src/main/java/com/malabudi/dineupbe/order/repository/LineItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.malabudi.dineupbe.order.repository;
-
-import com.malabudi.dineupbe.order.model.LineItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LineItemRepository extends JpaRepository<LineItem,Long> {}
