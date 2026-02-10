7759e68f8ae750712c3b4e59fdc09a76d12a34b5
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-02-09 19:20:05 -0500

    chore: removed unused exception file in menu package

--- src/main/java/com/malabudi/dineupbe/menu/exception/InvalidMenuGroupException.java
+++
@@ -1,9 +0,0 @@
-package com.malabudi.dineupbe.menu.exception;
-
-import com.malabudi.dineupbe.common.exception.ResourceBadRequestException;
-
-public class InvalidMenuGroupException extends ResourceBadRequestException {
-    public InvalidMenuGroupException(String message) {
-        super(message);
-    }
-}
