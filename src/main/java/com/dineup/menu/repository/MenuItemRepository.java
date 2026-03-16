0f4229ed10482bd682de03f15c1db6ed5282458e
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-03-16 13:11:42 -0400

    feat: added endpoint to fetch best selling menu items

--- src/main/java/com/dineup/menu/repository/MenuItemRepository.java
+++ src/main/java/com/dineup/menu/repository/MenuItemRepository.java
@@ -1,6 +1,23 @@
 package com.dineup.menu.repository;
 
+import com.dineup.menu.dto.MenuItemSalesDto;
 import com.dineup.menu.model.MenuItem;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> { }
+import java.util.List;
+
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+
+    @Query("""
+        SELECT li.menuItem.id AS menuItemId,
+               li.menuItem.name as menuItemName,
+               SUM(li.quantity) AS totalQuantitySold
+        FROM LineItem li
+        WHERE li.order.orderStatus = "COMPLETE"
+        GROUP BY li.menuItem.id, li.menuItem.name
+        ORDER BY totalQuantitySold DESC
+        """)
+    List<MenuItemSalesDto> findBestSellingMenuItems(Pageable pageable);
+}
